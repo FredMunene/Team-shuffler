3334deb3e758bacffa893ece5183891c83640d2d
fgitonga <mfredgitonga@gmail.com>
2024-06-23 19:53:38 +0300

    deleted files

--- src/routes/rules.go
+++
@@ -1,7 +0,0 @@
-package routes
-
-import "net/http"
-
-func RulesHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "templates/rules.html")
-}
--- src/types/configuration.go
+++
@@ -1,7 +0,0 @@
-package types
-
-type Config struct {
-	AppName string `json:"appname"`
-	HostName string `json:"hostname"`
-	HostPort int `json:"hostport"`
-}
